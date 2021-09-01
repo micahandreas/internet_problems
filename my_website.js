492a545724f09e70df3a643020351695b0fca6f3
micahandreas <64471090+micahandreas@users.noreply.github.com>
2021-09-01 17:47:56 +0800

    Add files via upload

---
+++ my_website.js
@@ -0,0 +1,3 @@
+function datapackGenerator() {
+    document.getElementById("datapackGenerator").innerHTML = "For Datapack Generators, you can make other stuff in your library of datapacks. <a href=\"https://notepad-plus-plus.org/\">Go to Notepad++</a> or <a href=\"https://code.visualstudio.org/\">VS Code</a> for More Datapack making tools. You can go to their code and for downloading their datapacks.";
+}
