c3441675cb36ec2c01bef50b8cc315db917cba06
micahandreas <64471090+micahandreas@users.noreply.github.com>
2021-09-02 11:10:48 +0800

    Delete my_website.js

--- my_website.js
+++
@@ -1,3 +0,0 @@
-function datapackGenerator() {
-    document.getElementById("datapackGenerator").innerHTML = "For Datapack Generators, you can make other stuff in your library of datapacks. <a href=\"https://notepad-plus-plus.org/\">Go to Notepad++</a> or <a href=\"https://code.visualstudio.com/\">VS Code</a> for More Datapack making tools. You can go to their code and for downloading their datapacks.";
-}
